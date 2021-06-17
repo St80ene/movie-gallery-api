0ef525a35bff5d01219a3f409919740735ce3aef
@st80ene <etienejames5@gmail.com>
2021-06-17 19:21:22 +0100

    edited .env file

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -3,23 +3,9 @@ const cloudinary = require('cloudinary').v2;
 
 
 cloudinary.config({
-	cloud_name: process.env.CLOUD_NAME || 'db14zb9ji',
-	api_key: process.env.CLOUD_API_KEY || '574885582731744',
-	api_secret: process.env.CLOUD_API_SECRET || '4_OxUWzHJ7FTNdx5Z0zr47JwKBg',
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUD_API_KEY,
+	api_secret: process.env.CLOUD_API_SECRET,
 });
-
-
-
-// exports.uploads = (file) => {
-// 	return new Promise((resolve) => {
-// 		cloudinary.uploader.upload(
-// 			file,
-// 			(result) => {
-// 				resolve({ url: result.url, id: result.public_id });
-// 			},
-// 			{ resource_type: 'video', chunk_size: 6000000 }
-// 		);
-// 	});
-// };
 
 module.exports = cloudinary;
